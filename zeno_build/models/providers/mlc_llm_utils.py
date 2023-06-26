46742429b2fec8260bd1a63b219b674619540468
Jinqi Chen <jinqic@alumni.cmu.edu>
2023-06-25 20:37:11 -0700

    get some chat interface ready

--- zeno_build/models/providers/mlc_llm_utils.py
+++ zeno_build/models/providers/mlc_llm_utils.py
@@ -15,6 +15,14 @@ from zeno_build.prompts.prompt_utils import replace_variables
 from mlc_llm.python.mlc_chat.chat_module import ChatModule
 
 
+def first_idx_mismatch(str1, str2):
+    """Find the first index that mismatch in two strings."""
+    for i, (char1, char2) in enumerate(zip(str1, str2)):
+        if char1 != char2:
+            return i
+    return min(len(str1), len(str2))
+
+
 def generate_from_mlc_llm(
     full_contexts: list[chat_prompt.ChatMessages],
     prompt_template: chat_prompt.ChatMessages,
@@ -41,10 +49,7 @@ def generate_from_mlc_llm(
 
     print(f"begin generate_from_mlc_llm")
 
-    # only full_contexts is useful
-
     chat_mod = ChatModule()
-
     artifact_path = "/root/dist"
 
     # reload the model
@@ -57,63 +62,45 @@ def generate_from_mlc_llm(
     assert lib is not None
     chat_mod.reload_func(lib, os.path.join(model_dir, "params"))
 
+    # chat interface
+    def get_output(stream_interval=2):
+        i, cur_utf8_chars = 0, "".encode("utf-8")
+        res = ""
+        while not chat_mod.stopped():
+            chat_mod.decode()
+            if i % stream_interval == 0 or chat_mod.stopped():
+                new_msg = chat_mod.get_message()
+                new_utf8_chars = new_msg.encode("utf-8")
+                pos = first_idx_mismatch(cur_utf8_chars, new_utf8_chars)
+                print_msg = ""
+                for _ in range(pos, len(cur_utf8_chars)):
+                    print_msg += "\b \b"
+                for j in range(pos, len(new_utf8_chars)):
+                    print_msg += chr(new_utf8_chars[j])
+                cur_utf8_chars = new_utf8_chars
+                res += print_msg
+        return res
+
+    results = []
+    for i in tqdm.trange(0, len(full_contexts)):
+        context = full_contexts[i]
+
+        if len(context.messages) == 0:
+            # reset the chat
+            chat_mod.reset_chat()
+            user_input = ""
+        else:
+            user_input = context.messages[-1].content
+
+        chat_mod.prefill(user_input)
+        res = get_output()
+        print(f"user input: {user_input}, llm response: {res}")
+
     import pdb
 
     pdb.set_trace()
 
-    # # Load model
-    # torch_device = "cuda" if torch.cuda.is_available() else "cpu"
-    # model_cls = (
-    #     model_config.model_cls
-    #     if model_config.model_cls is not None
-    #     else transformers.AutoModelForCausalLM
-    # )
-    # tokenizer_cls = (
-    #     model_config.tokenizer_cls
-    #     if model_config.tokenizer_cls is not None
-    #     else transformers.AutoTokenizer
-    # )
-    # model: transformers.PreTrainedModel = model_cls.from_pretrained(
-    #     model_config.model,
-    #     **model_config.model_loader_kwargs,
-    # ).to(torch_device)
-    # tokenizer: transformers.PreTrainedTokenizer = tokenizer_cls.from_pretrained(
-    #     model_config.model
-    # )
-    # tokenizer.padding_side = "left"
-    # if not tokenizer.pad_token:
-    #     tokenizer.pad_token = tokenizer.eos_token
-    #     tokenizer.pad_token_id = tokenizer.eos_token_id
-
-    # gen_config = transformers.GenerationConfig(
-    #     do_sample=True,
-    #     temperature=temperature,
-    #     max_new_tokens=max_tokens,
-    #     top_p=top_p,
-    #     eos_token_id=tokenizer.eos_token_id,
-    #     pad_token_id=tokenizer.pad_token_id,
-    # )
-    # # Create the prompts
-    # filled_prompts: list[str] = [
-    #     prompt_template.to_text_prompt(
-    #         full_context=full_context.limit_length(context_length),
-    #         name_replacements=model_config.name_replacements,
-    #     )
-    #     for full_context in full_contexts
-    # ]
-    # # Process in batches
-    # results = []
-    # batch_size = 8
     # for i in tqdm.trange(0, len(filled_prompts), batch_size):
-    #     batch_prompts = filled_prompts[i : i + batch_size]
-    #     encoded_prompts = tokenizer(
-    #         batch_prompts,
-    #         padding=True,
-    #         return_tensors="pt",
-    #         return_token_type_ids=False,
-    #     ).to(torch_device)
-    #     with torch.no_grad():
-    #         outputs = model.generate(**encoded_prompts, generation_config=gen_config)
     #     outputs = outputs[:, encoded_prompts["input_ids"].shape[-1] :]
     #     results.extend(tokenizer.batch_decode(outputs, skip_special_tokens=True))
     # # Post-processing to get only the system utterance
